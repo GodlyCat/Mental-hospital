b1565b9882775bf403a2b42ba56cd9a477bddb95
wWcheburekWw <gapdanil@yandex.ru>
2023-10-23 16:35:27 +0300

    Small explanation added

--- MentalHospital.DAL/Entities/Patient.cs
+++ MentalHospital.DAL/Entities/Patient.cs
@@ -10,7 +10,7 @@
         public DateTime RegistredAt { get; set; }
         public DateTime DeregistredAt { get; set; }
 
-        public string? PersonalDoctor { get; set; }
+        public string? PersonalDoctor { get; set; }  //doctor entity is not created yet
         public int? PersonalDoctorId { get; set; }
     }
 }

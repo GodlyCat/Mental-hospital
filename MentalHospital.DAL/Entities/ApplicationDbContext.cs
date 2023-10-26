5527e06da22ee063a35a2bde1a33ebbe399416b9
wWcheburekWw <gapdanil@yandex.ru>
2023-10-26 14:20:30 +0300

    DbContext added

---
+++ MentalHospital.DAL/Entities/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MentalHospital.DAL.Entities
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("connectionString");
+        }
+    }
+}

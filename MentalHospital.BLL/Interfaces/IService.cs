77d72c8f0996a2f8ee8fc4f9d0e5853cc372c43a
wWcheburekWw <gapdanil@yandex.ru>
2023-11-02 15:24:56 +0300

    Not used "usings" deleted

--- MentalHospital.BLL/Interfaces/IService.cs
+++ MentalHospital.BLL/Interfaces/IService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MentalHospital.BLL.Interfaces
+﻿namespace MentalHospital.BLL.Interfaces
 {
     public interface IService<T> where T : class
     {

8495bbf384012944fea4d1fac034f605c6e6f865
wWcheburekWw <gapdanil@yandex.ru>
2023-11-21 14:27:28 +0300

    Test project refactored

---
+++ MentalHospital.Tests/Data/Entities/PatientEntityData.cs
@@ -0,0 +1,25 @@
+﻿using MentalHospital.DAL.Entities;
+
+namespace MentalHospital.BLL.Tests.Data.Entities
+{
+    internal static class PatientEntityData
+    {
+        public static Patient InitPatientEntity()
+        {
+            var patient = new Patient()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Tom",
+                ChamberNumber = 1,
+                RegistredAt = DateTime.Now
+            };
+
+            return patient;
+        }
+
+        public static List<Patient> InitPatientCollection()
+        {
+            return new List<Patient>() { InitPatientEntity() };
+        }
+    }
+}
---
+++ MentalHospital.Tests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using Xunit;
+global using Moq;
+global using AutoMapper;
+global using Shouldly;
+
+global using MentalHospital.BLL.Models;
+global using MentalHospital.BLL.Services;
+global using MentalHospital.DAL.Interfaces;
+global using MentalHospital.DAL.Entities;

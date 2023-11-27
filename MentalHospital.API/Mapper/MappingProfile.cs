b26fcf19435579e8dc5ae7804abcc6bd9e6eb05b
wWcheburekWw <gapdanil@yandex.ru>
2023-11-27 15:44:56 +0300

    Mapper added to API layer

---
+++ MentalHospital.API/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MentalHospital.API.ViewModels;
+using MentalHospital.BLL.Models;
+
+namespace MentalHospital.API.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<PatientModel, PatientViewModel>().ReverseMap();
+        }
+    }
+}

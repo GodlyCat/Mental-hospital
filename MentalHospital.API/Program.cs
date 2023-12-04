ee8d7b37338215975f228246b92a48ef3ea8a185
ChebCheck <71707871+ChebCheck@users.noreply.github.com>
2023-12-04 14:54:36 +0300

    Fluent validation added (#34)
    * Fluent validation added
    
    * Update method validated

--- MentalHospital.API/Program.cs
+++ MentalHospital.API/Program.cs
@@ -1,8 +1,12 @@
 using MentalHospital.BLL.Extensions;
 using MentalHospital.API.Mapper;
+using MentalHospital.API.FluentValidation;
+using FluentValidation;
+using MentalHospital.API.ViewModels;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<IValidator<PatientViewModel>, PatientValidator>();
 builder.Services.AddAutoMapper(typeof(MappingProfile), typeof(MentalHospital.BLL.Mapper.MappingProfile));
 builder.Services.AddControllers();
 builder.Services.AddBusinessLayer(builder.Configuration);

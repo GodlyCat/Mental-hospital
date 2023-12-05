d12ee0e89d2c0b1dacfa75732f2affaf99c297bd
spiraspira <zabornik5350@gmail.com>
2023-12-05 17:31:30 +0300

    Updated Patient entity, updated Doctor entity

--- MentalHospital.DAL/Entities/Patient.cs
+++ MentalHospital.DAL/Entities/Patient.cs
@@ -3,15 +3,20 @@
 public class Patient
 {
 	public Guid Id {  get; set; }
-	public string? Name { get; set; }
-	public string? Diagnosis { get; set; }
-	public string? Therapy { get; set; }
-	public int ChamberNumber { get; set; }
-	public DateTime RegistredAt { get; set; }
-	public DateTime DeregistredAt { get; set; }
 
-	public string? PersonalDoctor { get; set; }  //doctor entity is not created yet
-	public int? PersonalDoctorId { get; set; }
+	public string? FirstName { get; set; }
+
+	public string? LastName { get; set; }
+
+	public string? Diagnosis { get; set; }
+
+	public string? Therapy { get; set; }
+
+	public int? ChamberNumber { get; set; }
+
+	public DateTime RegisteredAt { get; set; }
+
+	public DateTime UnregisteredAt { get; set; }
 
 	public List<DoctorPatient> DoctorPatients { get; set; } = new();
 

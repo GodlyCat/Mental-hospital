03f49a9906b1d8e821c461182f4bdb393da3decb
spiraspira <zabornik5350@gmail.com>
2023-12-05 19:21:29 +0300

    Updated PatientValidator to apply updated PatientViewModel

--- MentalHospital.API/FluentValidation/PatientValidator.cs
+++ MentalHospital.API/FluentValidation/PatientValidator.cs
@@ -9,13 +9,13 @@ namespace MentalHospital.API.FluentValidation
     {
         public PatientValidator() 
         {
-            RuleFor(p => p.Name).Length(2, 50);
+            RuleFor(p => p.FirstName).Length(2, 50);
             RuleFor(p => p.ChamberNumber).ExclusiveBetween(0, 1000);
             RuleFor(p => p.Diagnosis).Length(1, 100);
             RuleFor(p => p.Therapy).Length(1, 150);
-            RuleFor(p => p.RegistredAt)
+            RuleFor(p => p.RegisteredAt)
                 .Must(dt => DateTime.TryParse(dt.ToString(), out dt));
-            RuleFor(p => p.DeregistredAt)
+            RuleFor(p => p.UnregisteredAt)
                 .Must(dt => DateTime.TryParse(dt.ToString(), out dt));
 
         }

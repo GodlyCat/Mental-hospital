97bd057726fa6fd3ac610b60630e23f192c5a37b
spiraspira <zabornik5350@gmail.com>
2023-12-05 19:24:22 +0300

    Fixed formatting

--- MentalHospital.DAL/Entities/ApplicationDbContext.cs
+++ MentalHospital.DAL/Entities/ApplicationDbContext.cs
@@ -1,18 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace MentalHospital.DAL.Entities
+namespace MentalHospital.DAL.Entities;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
-    {
-	    public DbSet<Doctor> Doctors { get; set; }
+	public DbSet<Doctor> Doctors { get; set; }
 
-		public DbSet<Patient> Patients { get; set; }
+	public DbSet<Patient> Patients { get; set; }
 
-        public DbSet<Session> Sessions { get; set; }
+	public DbSet<Session> Sessions { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-            Database.EnsureCreated();
-        }
-    }
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+		Database.EnsureCreated();
+	}
 }

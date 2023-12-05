528550818d0f753cbbc03a56942981989a83e4b2
spiraspira <zabornik5350@gmail.com>
2023-12-05 17:24:46 +0300

    Extended doctor entity with properties

--- MentalHospital.DAL/Entities/Doctor.cs
+++ MentalHospital.DAL/Entities/Doctor.cs
@@ -4,7 +4,13 @@ public class Doctor
 {
 	public Guid Id { get; set; }
 
-	public string? Name { get; set; }
+	public string? FirstName { get; set; }
+
+	public string? LastName { get; set; }
+
+	public string? AppointmentTime { get; set; }
+
+	public int? RoomNumber { get; set; }
 
 	public List<DoctorPatient> DoctorPatients { get; set; } = new();
 
--- MentalHospital.DAL/Entities/Patient.cs
+++ MentalHospital.DAL/Entities/Patient.cs
@@ -1,20 +1,19 @@
-﻿namespace MentalHospital.DAL.Entities
+﻿namespace MentalHospital.DAL.Entities;
+
+public class Patient
 {
-    public class Patient
-    {
-        public Guid Id {  get; set; }
-        public string? Name { get; set; }
-        public string? Diagnosis { get; set; }
-        public string? Therapy { get; set; }
-        public int ChamberNumber { get; set; }
-        public DateTime RegistredAt { get; set; }
-        public DateTime DeregistredAt { get; set; }
+	public Guid Id {  get; set; }
+	public string? Name { get; set; }
+	public string? Diagnosis { get; set; }
+	public string? Therapy { get; set; }
+	public int ChamberNumber { get; set; }
+	public DateTime RegistredAt { get; set; }
+	public DateTime DeregistredAt { get; set; }
 
-        public string? PersonalDoctor { get; set; }  //doctor entity is not created yet
-        public int? PersonalDoctorId { get; set; }
+	public string? PersonalDoctor { get; set; }  //doctor entity is not created yet
+	public int? PersonalDoctorId { get; set; }
 
-        public List<DoctorPatient> DoctorPatients { get; set; } = new();
+	public List<DoctorPatient> DoctorPatients { get; set; } = new();
 
-        public List<Doctor> Doctors { get; set; } = new();
-    }
+	public List<Doctor> Doctors { get; set; } = new();
 }

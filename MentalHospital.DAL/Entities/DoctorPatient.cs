9caf4ec6ea212eafe2166d215fe1221789cc07d8
spiraspira <zabornik5350@gmail.com>
2023-12-05 13:59:33 +0300

    Added intermediate entity DoctorPatient

---
+++ MentalHospital.DAL/Entities/DoctorPatient.cs
@@ -0,0 +1,14 @@
+﻿namespace MentalHospital.DAL.Entities;
+
+public class DoctorPatient
+{
+	public int Id { get; set; }
+
+	public int DoctorId { get; set; }
+
+	public Doctor? Doctor { get; set; }
+
+	public int PatientId { get; set; }
+
+	public Patient? Patient { get; set; }
+}

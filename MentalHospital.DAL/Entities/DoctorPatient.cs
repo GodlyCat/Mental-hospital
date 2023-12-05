28f18363a5e8f6e3de2afcd002fbc34e48fc15b1
spiraspira <zabornik5350@gmail.com>
2023-12-05 14:50:20 +0300

    Changed Ids type to string

--- MentalHospital.DAL/Entities/DoctorPatient.cs
+++ MentalHospital.DAL/Entities/DoctorPatient.cs
@@ -2,13 +2,13 @@
 
 public class DoctorPatient
 {
-	public int Id { get; set; }
+	public string Id { get; set; }
 
-	public int DoctorId { get; set; }
+	public string DoctorId { get; set; }
 
 	public Doctor? Doctor { get; set; }
 
-	public int PatientId { get; set; }
+	public string PatientId { get; set; }
 
 	public Patient? Patient { get; set; }
 }

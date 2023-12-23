f48aca421b8db53fa13105389ee2919cd15bcb24
spiraspira <zabornik5350@gmail.com>
2023-12-23 15:49:10 +0300

    Added doctor and session services

---
+++ MentalHospital.BLL/Services/SessionService.cs
@@ -0,0 +1,8 @@
+﻿namespace MentalHospital.BLL.Services;
+
+public class SessionService : GenericService<SessionModel, Session>, ISessionService
+{
+	public SessionService(IGenericRepository<Session> repository, IMapper maper) : base(repository, maper)
+	{
+	}
+}
